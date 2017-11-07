5435439821991ae921762ef8741453ecf1180823
Antti Pöyhönen <anttipoyhonen1986@gmail.com>
2017-11-07 11:06:22 +0200

    dbscript

---
+++ src/Yhteys.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Yhteys", urlPatterns = {"/Yhteys"})
+public class Yhteys extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
