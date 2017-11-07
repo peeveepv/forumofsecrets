17aa2a525489dd731b6adc4ad605941c95ea889c
Mika Le Gall <mika.legall@live.fi>
2017-11-07 15:55:33 +0200

    talteen

--- src/Foorumi/HakuServlet.java
+++ src/Foorumi/HakuServlet.java
@@ -2,25 +2,78 @@ package Foorumi;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 @WebServlet(name = "Hakukone", urlPatterns = {"/hakukone"})
 public class HakuServlet extends HttpServlet {
 
-    public static String vapaaHaku (Connection con, String haettava) {
 
-        String sql = "select otsikko from viesti";
 
-        PreparedStatement kyselyLause = con.prepareStatement(sql);
-//        kyselyLause.setString(1, haettava);
-        ResultSet kyselynTulos = kyselyLause.executeQuery();
+    public static void main(String[] args) {
 
-        while(kyselynTulos.next()) {
+        Connection con = mockiMetodiConnectionille();
 
+        String testi = vapaaHaku(con, "kukkaruukku");
+
+        System.out.println(testi);
+    }
+
+
+    public static Connection mockiMetodiConnectionille() {
+
+        Connection con = null;
+
+        try {
+
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/foorumi?useSSL=false",
+                    "root", "jaavakahvi");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-        return "";
+        System.out.println("Yhteys onnistui");
+
+        return con;
+    }
+
+
+
+//    ************************************************
+
+
+
+
+
+
+
+
+
+    public static String vapaaHaku (Connection con, String haettava) {
+
+        try {
+
+            String sql1 = "select nimi from keskustelu";
+            String sql2 = "select kuvaus from keskustelu";
+
+            String sql3 = "select kirjoittaja from viesti";
+            String sql4 = "select otsikko from viesti";
+            String sql5 = "select viesti from viesti";
+            String sql6 = "select vastaus from viesti";
+
+            PreparedStatement kyselyLause1 = con.prepareStatement(sql1);
+            ResultSet kyselynTulos1 = kyselyLause1.executeQuery();
+
+
+            while (kyselynTulos1.next()) {
+                String testi = kyselynTulos1.getString("name");
+                System.out.println(testi);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "joo";
+
     }
 }
