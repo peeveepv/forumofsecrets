4e03c1701bf1468f8c99117b7bff0b8322e46a45
Mika Le Gall <mika.legall@live.fi>
2017-11-07 15:29:02 +0200

    hakukone luotu (ei toimi)

--- src/Foorumi/HakuServlet.java
+++ src/Foorumi/HakuServlet.java
@@ -1,18 +1,25 @@
 package Foorumi;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class Haku {
+@WebServlet(name = "Hakukone", urlPatterns = {"/hakukone"})
+public class HakuServlet extends HttpServlet {
 
     public static String vapaaHaku (Connection con, String haettava) {
 
-        String sql = "select salasana from henkilo where kayttajanimi like ?";
+        String sql = "select otsikko from viesti";
 
         PreparedStatement kyselyLause = con.prepareStatement(sql);
-        kyselyLause.setString(1, haettava);
+//        kyselyLause.setString(1, haettava);
         ResultSet kyselynTulos = kyselyLause.executeQuery();
+
+        while(kyselynTulos.next()) {
+
+        }
 
         return "";
     }
