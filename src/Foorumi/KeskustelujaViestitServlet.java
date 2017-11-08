697113229b50ccd1d658ff8bca1074f1f618c647
MinnaVares <minna.vares@gmail.com>
2017-11-08 16:03:19 +0200

    Ottakaa tästä oma Navipalkki

--- src/Foorumi/KeskustelujaViestitServlet.java
+++ src/Foorumi/KeskustelujaViestitServlet.java
@@ -22,6 +22,7 @@ public class KeskustelujaViestitServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         response.setContentType("text/html");
+
         PrintWriter out = null;
         Connection con = null;
         String sql = "INSERT INTO keskustelu (nimi, kuvaus) VALUES (?, ?)";
@@ -48,74 +49,7 @@ public class KeskustelujaViestitServlet extends HttpServlet {
         HttpSession session = request.getSession(false);
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-
-//Tämä tulostaa keskustelualustan pohjan
-        response.setContentType("text/html");
-
-        out.println("<html>");
-        out.println("<head>");
-
-        out.println("<title>Profiili</title>");
-
-        out.println(
-                "<style> td {word-break: break-all; } " +
-                        "#content {position: relative; left: 260px; width: 80%;} " +
-                        "#content {position: relative; left: 260px; width: 80%;} " +
-                        "nav {position: fixed; top: 0; width: 240px; height: 100%; font-family: Georgia; " +
-                        "background-color: #333; float: left; clear: left; display: inline; } " +
-                        "nav a, nav span {display: block; padding: 14px 16px; color: antiquewhite; text-shadow: none; " +
-                        "text-decoration: none;} .active {background-color: dimgrey;} " +
-                        "nav a:active, nav a:visited {color: antiquewhite; text-shadow: none;} " +
-                        "nav a:hover {background-color: #111;} " +
-                        "</style>"
-        );
-
-        out.println("</head>");
-
-        out.println("<body>");
-
-        out.println(
-                "<nav> " +
-                        "<span></span>" +
-                        "<span style='font-size: 120%'><a href='index.jsp'><strong>Forum of Secrets</strong></a></span>" +
-                        "<span></span>" +
-                        "<a href='/KeskustelujaViestitServlet'>Keskustelut</a>" +
-                        "<span></span>"
-        );
-
-        if (session == null
-                || session.getAttribute("kayttajanimi") == null
-                || "anonymous".equals(session.getAttribute("kayttajanimi"))) {
-
-            out.println("<a href='/Login'>Kirjautuminen</a>");
-            out.println("<a href='/Kayttaja'>Rekisteröityminen</a>");
-
-        } else {
-
-            out.println("<span style='font-size: 80%'><i>Tällä hetkellä kirjautuneena:</i>");
-
-            if (session.getAttribute("nimimerkki") == null) {
-                out.println(session.getAttribute("kayttajanimi"));
-            } else {
-                out.println(session.getAttribute("nimimerkki"));
-            }
-
-            out.println("</span>");
-
-            out.println("<a href='/Profiili'>Profiili</a>");
-            out.println("<a href='/Logout'>Uloskirjautuminen</a>");
-
-        }
-
-        out.println(
-                "<span></span>" +
-                        "<a href='/Hakukone'>Etsi viestejä</a>" +
-                        "<span></span>" +
-                        "</nav>" +
-                        "" +
-                        "<div id='content'>"
-        );
-
+        NaviPalkki.luoNaviPalkki(request, response, "Keskustelut");
         haeKeskustelut(request, response);
             out.println("<form method='post' >");
             out.println("<input type=text name=nimi value=nimi> </br>");
@@ -123,7 +57,7 @@ public class KeskustelujaViestitServlet extends HttpServlet {
             out.println("<input type=submit  value='Aloita uusi keskustelu'>");
             out.println("</form>");
 
-            out.println("</div>");
+            out.println("</div>"); //Tämä sulkee haeNaviPalkkimetodin
     }
 
 
@@ -151,6 +85,7 @@ public class KeskustelujaViestitServlet extends HttpServlet {
             out.print("</ul>");
         } catch (IOException e) {
             out.print(e.getMessage());
+        }
     }
-    }
+
 }
--- src/Foorumi/NaytaKeskustelu.java
+++ src/Foorumi/NaytaKeskustelu.java
@@ -109,72 +109,7 @@ public class NaytaKeskustelu extends HttpServlet {
                     );
                 }
 
-                //Tämä tulostaa keskustelualustan pohjan
-                res.setContentType("text/html");
-
-                out.println("<html>");
-                out.println("<head>");
-
-                out.println("<title>Kaikki viestit</title>");
-
-                out.println(
-                    "<style> td {word-break: break-all; } " +
-                            "#content {position: relative; left: 260px; width: 80%;} " +
-                            "#content {position: relative; left: 260px; width: 80%;} " +
-                            "nav {position: fixed; top: 0; width: 240px; height: 100%; font-family: Georgia; " +
-                            "background-color: #333; float: left; clear: left; display: inline; } " +
-                            "nav a, nav span {display: block; padding: 14px 16px; color: antiquewhite; text-shadow: none; " +
-                            "text-decoration: none;} .active {background-color: dimgrey;} " +
-                            "nav a:active, nav a:visited {color: antiquewhite; text-shadow: none;} " +
-                            "nav a:hover {background-color: #111;} " +
-                    "</style>"
-                );
-
-                out.println("</head>");
-
-                out.println("<body>");
-
-                out.println(
-                    "<nav> " +
-                            "<span></span>" +
-                            "<span style='font-size: 120%'><a href='index.jsp'><strong>Forum of Secrets</strong></a></span>" +
-                            "<span></span>" +
-                            "<a href='/KeskustelujaViestitServlet'>Keskustelut</a>" +
-                            "<span></span>"
-                );
-
-                if (session == null
-                        || session.getAttribute("kayttajanimi") == null
-                        || "anonymous".equals(session.getAttribute("kayttajanimi"))) {
-
-                    out.println("<a href='/Login'>Kirjautuminen</a>");
-                    out.println("<a href='/Kayttaja'>Rekisteröityminen</a>");
-
-                } else {
-
-                    out.println("<span style='font-size: 80%'><i>Tällä hetkellä kirjautuneena:</i>");
-
-                    if (session.getAttribute("nimimerkki") == null) {
-                        out.println(session.getAttribute("kayttajanimi"));
-                    } else {
-                        out.println(session.getAttribute("nimimerkki"));
-                    }
-
-                    out.println("</span>");
-
-                    out.println("<a href='/Profiili'>Profiili</a>");
-                    out.println("<a href='/Logout'>Uloskirjautuminen</a>");
-
-                }
-
-                out.println(
-                    "<span></span>" +
-                            "<a href='/Hakukone'>Etsi viestejä</a>" +
-                            "<span></span>" +
-                    "</nav>" +
-                    "" +
-                    "<div id='content'>"
-                );
+                NaviPalkki.luoNaviPalkki(req, res, "Viestit");
 
                 out.println("<h1>Keskustelu:<br> " + keskustelunimi + "</h1>");
                 out.println("<h3>Kuvaus: <i>" + keskustelukuvaus + "</i></h3>");
