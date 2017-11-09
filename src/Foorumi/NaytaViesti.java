9bdc06519f235ed53b5df0ff6d4cb8f2e0fe9c67
Antti Pöyhönen <anttipoyhonen1986@gmail.com>
2017-11-09 14:57:45 +0200

    login onnistumisen ilmoitus tyylitys ja linkkien alaviivat pois

---
+++ src/Foorumi/NaytaViesti.java
@@ -0,0 +1,20 @@
+package Foorumi;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "NaytaViesti", urlPatterns = {"/NaytaViesti"})
+public class NaytaViesti extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
